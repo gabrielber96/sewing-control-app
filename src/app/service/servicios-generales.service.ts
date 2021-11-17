cceabde4c6196cadc6ca51bef945e11a5f376bbe
Bryan Ortiz <bryan.ortiz@holinsys.pe>
2021-11-17 10:45:23 -0500

    Import service

---
+++ src/app/service/servicios-generales.service.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ServiciosGenerales {
+  // API path
+  URL_BACKEND = environment.BACKEND_URL;
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private menu: MenuController
+  ) {}
+
+  // Http Options
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    }),
+  };
+
+  signout() {
+    let headers = new HttpHeaders();
+    headers = headers.set('Content-type', 'application/json');
+    headers = headers.set(
+      'Authorization',
+      'Bearer ' + localStorage.getItem('cofide_token')
+    );
+    this.http
+      .get(this.URL_BACKEND + '/api/signout', {
+        headers: headers,
+      })
+      .subscribe(
+        (resp) => {
+          this.menu.close();
+          localStorage.removeItem('cofide_token');
+          localStorage.clear();
+          this.router.navigateByUrl('/login');
+        },
+        (error) => {
+          this.menu.close();
+          localStorage.removeItem('cofide_token');
+          localStorage.clear();
+          this.router.navigateByUrl('/login');
+        }
+      );
+  }
+
+  RegistrarUser(customer): Observable<any> {
+    let headers = new HttpHeaders();
+    headers = headers.set('Content-type', 'application/json');
+    headers = headers.set(
+      'Authorization',
+      'Basic ' + localStorage.getItem('cofide_token')
+    );
+
+    return this.http.post(this.URL_BACKEND + '/api/signup', customer, {
+      headers: headers,
+    });
+  }
+
+  loginNormal(email, password): Observable<any> {
+    // console.log(token)
+
+    return this.http.post(this.URL_BACKEND + '/api/signin', {
+      email,
+      password,
+    });
+  }
+}

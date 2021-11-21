a4306b28d9a085ac6df740ee8172e201d1ab9459
Bryan Ortiz <bryan.ortiz@holinsys.pe>
2021-11-21 13:38:06 -0500

    Fix buxed menu  Button tabs 2 addesd

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,10 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
-
   constructor() {}
 
+  fetchData2 = [
+    { name: 'Test 2', link: '' },
+
+    { name: 'Test 1', link: '' },
+    { name: 'Test 3', link: '' },
+
+    { name: 'Test 4', link: '' },
+  ];
+  ngOnInit(): void {
+    console.log(this.fetchData2);
+  }
 }

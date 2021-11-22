d926d8f6ab9eb585b7672ad3f79e41e0013a5d8b
Bryan Ortiz <bryan.ortiz@holinsys.pe>
2021-11-21 22:53:36 -0500

    fixed

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -12,14 +12,42 @@ export class Tab2Page {
     console.log(this.userRol, 'obteniendo numero en compoennte 2');
   }
   userRol;
+  fetchData2;
 
-  fetchData2 = [
-    { name: 'Test 2', link: '' },
+  ngOnInit(): void {
+    if (this.userRol == 1) {
+      this.fetchData2 = [
+        { name: 'Empleados', ico: '', link: '' },
+        { name: 'Pagos', ico: '', link: '' },
+        { name: 'Metricas', ico: '', link: '' },
+      ];
+    }
+    if (this.userRol == 2) {
+      this.fetchData2 = [
+        { name: 'Inventario', ico: '', link: '' },
 
-    { name: 'Test 1', link: '' },
-    { name: 'Test 3', link: '' },
-
-    { name: 'Test 4', link: '' },
-  ];
-  ngOnInit(): void {}
+        { name: 'Gestion de tareas', ico: '', link: '' },
+      ];
+    }
+    if (this.userRol == 3) {
+      this.fetchData2 = [
+        { name: 'Validación de tareas', ico: '', link: '' },
+        { name: 'Reporte de tareas', ico: '', link: '' },
+      ];
+    }
+    if (this.userRol == 4) {
+      this.fetchData2 = [
+        { name: 'Inventario', ico: '', link: '' },
+        { name: 'Gestion de tareas', ico: '', link: '' },
+        { name: 'Validación de tareas', ico: '', link: '' },
+        { name: 'Reporte de tareas', ico: '', link: '' },
+      ];
+    }
+    if (this.userRol == 5) {
+      this.fetchData2 = [
+        { name: 'Tareas', ico: '', link: '' },
+        { name: 'Tareas completadas', ico: '', link: '' },
+      ];
+    }
+  }
 }
